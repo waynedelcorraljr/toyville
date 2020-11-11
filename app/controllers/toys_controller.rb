e9a4aa84757793dc91164ee30998fabfcc6bfbb9
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-11-10 23:54:59 -0600

    finishes new toy, adds edit toy, adds delete toy

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,11 +1,11 @@
 class ToysController < ApplicationController
 
     get '/toys/new' do
-        binding.pry
-        if is_logged_in?(session)
-            erb :'/toys/new'
+        # binding.pry
+        if !is_logged_in?(session)
+            redirect '/login'
         end
-        redirect '/login'
+        erb :'/toys/new'
     end
     
     get '/toys/:id' do
@@ -13,9 +13,36 @@ class ToysController < ApplicationController
         erb :'/toys/show'
     end
 
+    get '/toys/:id/edit' do
+        @toy = Toy.find(params[:id])
+        if is_logged_in?(session) && current_user.id == @toy.user_id
+            erb :'/toys/edit'
+        else
+            redirect '/login'
+        end
+    end
+
     post '/toys/new' do
         toy = Toy.create(name: params[:name], category: params[:category], user_id: current_user.id)
         redirect "/toys/#{toy.id}"
+    end
+
+    patch '/toys/:id' do 
+        toy = Toy.find(params[:id])
+        if !params[:name].empty? && !params[:category].empty?
+            toy.update(name: params[:name], category: params[:category])
+            redirect "/toys/#{toy.id}"
+        else
+            redirect "/toys/#{toy.id}/edit"
+        end
+    end
+
+    delete '/toys/:id' do
+        toy = Toy.find(params[:id])
+        if is_logged_in?(session) && toy.user_id == session[:user_id]
+            toy.delete
+        end
+        redirect "/users/#{current_user.slug}"
     end
 
     helpers do
